add841235ff99eb7a94de6be7b63929f0894323e
luigi.cardamone <luigi.cardamone@ped.technology>
2020-04-02 10:44:42 +0200

    Add other exercises and solutions

---
+++ lesson-php/exercise-for2.php
@@ -0,0 +1,12 @@
+<?php
+
+// Esercizio:
+// Leggere un numero N positivo, e stampare a video tutti i numeri Pari maggiori di 0 e minori di N.
+// Esempio: dato N = 10 l’algoritmo deve dare come output: 0, 2, 4, 6, 8
+
+// Suggerimento:
+// Usare l'operatore % (operatore modulo, torna il resto della divisione intera)
+// Esempio: 5 % 2 torna 1, 4 % 2 torna 0
+
+// TODO
+
